cf182a15b47996cee883c783eefd13c8dc7a2b9d
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 17:32:08 +0300

    Import

---
+++ MaximumElementsStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumElementsStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+            var list = new List<int>();
+
+            for (int i = 0; i < number; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                var type = input[0];
+                
+                switch (type)
+                {
+                    case "1":
+                        var numberToPush = int.Parse(input[1]);
+                        stack.Push(numberToPush);
+                        break;
+                    case "2":
+                        stack.Pop();
+                        break;
+                    case "3":
+                        var maxElement = stack.Max();
+                        list.Add(maxElement);
+                        break;
+                }
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
